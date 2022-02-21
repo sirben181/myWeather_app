f2dfabc04f90e0d0ce2fbe045873bccd634f9b83
sirben181 <bensonkamauben@gmail.com>
2022-02-21 23:13:19 +0300

    breakdown

--- client/script.js
+++ client/script.js
@@ -1,3 +1,5 @@
+const res = require("express/lib/response")
+
 const city=document.querySelector('.city')
 const results=document.querySelector('.results')
 const btn=document.querySelector('.btn')
@@ -7,3 +9,9 @@ city.target.value;
 console.log(city.target.value)
 
 })
+const fetchWeather=async()=>{
+    const weather=await fetch('http://localhost:6000/posts')
+    const res=await res.json()
+    console.log(res)
+}
+fetchWeather()
