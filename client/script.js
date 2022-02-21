b63ae0850304c91d4a91e5683be80eda9ef543d3
sirben181 <bensonkamauben@gmail.com>
2022-02-21 22:59:47 +0300

    hey

---
+++ client/script.js
@@ -0,0 +1,9 @@
+const city=document.querySelector('.city')
+const results=document.querySelector('.results')
+const btn=document.querySelector('.btn')
+btn.addEventListener('click',(e)=>{
+e.preventDefault()
+city.target.value;
+console.log(city.target.value)
+
+})
