c4943cf3ccf608b93950ab7f24b867a9685c8483
sirben181 <bensonkamauben@gmail.com>
2022-02-23 10:16:07 +0300

    fetched weather form the backend

--- client/script.js
+++ client/script.js
@@ -1,4 +1,3 @@
-const res = require("express/lib/response")
 
 const city=document.querySelector('.city')
 const results=document.querySelector('.results')
@@ -10,8 +9,35 @@ console.log(city.target.value)
 
 })
 const fetchWeather=async()=>{
-    const weather=await fetch('http://localhost:6000/posts')
-    const res=await res.json()
-    console.log(res)
+
+    const res=await fetch('http://localhost:5000/posts')
+    const data=await res.json()
+    
+    const displayData={
+        city:data.name,
+        temp:kelvinToCelsius(data.temp),
+        lat:data.coord.lat,
+        lon:data.coord.lon,
+        visibility:data.visibility,
+        windspeed:data.wind.speed,
+        sunrise:(data.sunrise),
+        sunset:data.sunset
+
+    }
+    console.log(data.sunrise)
+
+    AddData(displayData)
 }
 fetchWeather()
+const AddData=(data)=>{
+results.innerHTML=`
+<h1>The weather in ${data.city} of lat ${data.lat}& long ${data.lon} is: </h1>
+<p>Temperature is ${data.temp}&deg;C </p>
+<p>with a visibility of ${data.visibility}</p>
+<p>wind speed is ${data.windspeed} miles/hr</p>
+`
+}
+const kelvinToCelsius =(temp)=>{
+    return Math.ceil((temp-273.15))
+
+}
--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,34 @@ dotenv.config({path:'./config/config.env'})
 const router=express.Router()
 
 
- router.route('/').get((req,res)=>{
- res.send('<h1>welcome to this home </h1>')
- })
+ router.route('/').get()
+ 
  const appkey=process.env.API_KEY
 
  let city='london'
  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appkey}`)
  .then(response=> {
+     const name=response.data.name
     const temp=response.data.main.temp
+    const pressure=response.data.main.pressure
     const coord=response.data.coord
     const humidity=response.data.main.humidity
     const wind=response.data.wind
     const rain=response.data.rain
     const visibility=response.data.visibility
+    const sunrise=response.data.sys.sunrise
+    const sunset=response.data.sys.sunset
+    // console.log(sunset)
     const weather=({
+        name,
         temp,
         coord,
         humidity,
         wind,
         rain,
         visibility,
+        sunrise,
+        sunset
 
     })
  router.route('/posts').get((req,res)=>{
